a6d8f0b957be89aec1e1e490cf0157743f8139aa
Kalliana Glaeser <kalliana.glaeser@gmail.com>
2025-04-19 10:26:13 -0300

    exrcicios javascript basico

---
+++ javascript/javascript-basico/Exercicio01.js
@@ -0,0 +1,101 @@
+console.log("************************************************")
+//Declare uma variável usando var , outra com let e outra com const . Modifique
+// o valor das variáveis declaradas com var e let . Tente alterar a variável const e
+// observe o que acontece.
+
+
+
+
+
+console.log("************************************************")
+//Crie uma variável do tipo number e outra do tipo string . Converta a variável do
+// tipo number para string e a do tipo string para number .
+
+
+
+
+
+
+console.log("************************************************")
+//Defina duas variáveis booleanas ( true e false ) e use operadores lógicos (AND,
+// OR, NOT) para criar diferentes combinações de resultados.
+
+
+
+
+console.log("************************************************")
+//Escreva um código que use uma estrutura condicional ( if/else ) para verificar
+// se um número é positivo, negativo ou zero.
+
+
+
+
+console.log("************************************************")
+//Utilize o operador ternário para verificar se uma idade é maior ou igual a 18.
+// Exiba "Maior de idade" ou "Menor de idade" com base na condição.
+
+
+
+
+
+console.log("************************************************")
+//Crie um laço for que exiba todos os números de 1 a 10 no console.
+
+
+
+
+console.log("************************************************")
+//Usando um laço while , some os números de 1 a 100 e exiba o resultado final no
+// console.
+
+
+
+
+
+console.log("************************************************")
+//Declare uma função que receba dois números como parâmetros e retorne a
+// soma deles. Chame essa função e exiba o resultado no console.
+
+
+
+console.log("************************************************")
+//Crie um array com os nomes de 5 frutas. Exiba o terceiro item do array no
+// console. Depois, adicione uma fruta ao final do array e remova a primeira fruta.
+// Exiba o array atualizado.
+
+
+
+
+console.log("************************************************")
+//Crie um objeto que represente uma pessoa, contendo as propriedades nome ,
+// idade e profissão . Acesse e exiba o valor da propriedade nome .
+
+
+
+console.log("************************************************")
+//Adicione uma nova propriedade chamada hobby ao objeto criado no exercício
+// anterior. Atualize o valor da propriedade idade para um número diferente.
+
+
+
+console.log("************************************************")
+//Crie uma função que receba um array de números e retorne apenas os
+// números pares.
+
+
+
+
+console.log("************************************************")
+//Faça um programa que use uma estrutura de repetição ( for ) para calcular o
+// fatorial de um número previamente definido.
+
+
+
+
+console.log("************************************************")
+//Defina dois objetos que representem livros, contendo título , autor e ano de
+// publicação . Crie uma função que compare os anos de publicação e determine
+// qual livro foi publicado primeiro.
+
+
+
