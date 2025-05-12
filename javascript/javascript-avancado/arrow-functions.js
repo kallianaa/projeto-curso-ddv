4bd9b5066ec4af64b4583747d4834833eae46105
Kalliana Glaeser <kalliana.glaeser@gmail.com>
2025-05-11 22:24:57 -0300

    js

---
+++ javascript/javascript-avancado/arrow-functions.js
@@ -0,0 +1,12 @@
+/*
+const pessoa = {
+    nome: 'Nala',
+    saudacao: () => {
+        console.log(this);
+    }
+}
+
+pessoa.saudacao()*/
+
+const quadrado = n =>  n * n;
+console.log(quadrado(2));
