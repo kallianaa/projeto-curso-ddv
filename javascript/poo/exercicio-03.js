df8c8742e881609c67f1fc313316fae1dda02d22
kallianaa <kalliana.glaeser@gmail.com>
2025-06-23 20:20:50 -0300

    Create exercicio-03.js

---
+++ javascript/poo/exercicio-03.js
@@ -0,0 +1,49 @@
+console.log("************************************************")
+console.log("EXERCÍCIO 1")
+//Crie uma classe Livro com propriedades como titulo, autor e ano.
+// Adicione um método que exibe uma mensagem com os detalhes do livro no console.
+
+
+
+console.log("************************************************")
+console.log("EXERCÍCIO 2")
+//Implemente um sistema de herança com as classes Funcionario, Gerente e Diretor.
+// Adicione uma propriedade salario e sobrescreva um método para calcular bônus em cada classe.
+
+
+
+console.log("************************************************")
+console.log("EXERCÍCIO 3")
+//Crie uma classe Cliente que possui um método para calcular o valor total de compras,
+// com base em uma lista de produtos comprados.
+
+
+
+console.log("************************************************")
+console.log("EXERCÍCIO 4")
+//Crie uma classe Jogador com propriedades como nome, nivel e pontos.
+// Adicione um método que aumenta os pontos do jogador com base em ações realizadas.
+
+
+
+console.log("************************************************")
+console.log("EXERCÍCIO 5")
+//Sistema de Reservas de Hotel:
+// Crie uma classe Hotel que possui uma lista de quartos (Quarto).
+// Cada quarto deve ter um número, capacidade e status (livre ou ocupado).
+// Adicione um método para reservar um quarto, verificando se há disponibilidade.
+// Implemente outro método que exibe a lista de quartos disponíveis e ocupados.
+// Crie uma classe Cliente que armazene os dados dos clientes e associe cada cliente ao quarto reservado.
+
+
+
+console.log("************************************************")
+console.log("EXERCÍCIO 6")
+//Gerenciador de Tarefas com Prioridades:
+// Desenvolva uma classe Tarefa com propriedades como descricao, status (pendente ou concluída) e
+// prioridade (baixa, média, alta).
+// Crie uma classe GerenciadorDeTarefas que contém uma lista de tarefas.
+// Adicione métodos para adicionar, remover e listar tarefas por prioridade.
+//Implemente um método para marcar uma tarefa como concluída e outro para exibir apenas as tarefas pendentes.
+
+
